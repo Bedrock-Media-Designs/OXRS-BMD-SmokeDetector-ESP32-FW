7ae825e4213a35a42a9bd12aba4f49a3b32efd10
Ben Jones <ben.jones12@gmail.com>
2021-10-27 16:08:00 +1300

    increment version to 3.0.0

--- OXRS-BMD-SmokeDetector-ESP32-FW.ino
+++ OXRS-BMD-SmokeDetector-ESP32-FW.ino
@@ -27,7 +27,7 @@
 #define FW_NAME       "OXRS-BMD-SmokeDetector-ESP32-FW"
 #define FW_SHORT_NAME "Smoke Detector"
 #define FW_MAKER      "Bedrock Media Designs"
-#define FW_VERSION    "2.1.0"
+#define FW_VERSION    "3.0.0"
 
 /*--------------------------- Libraries ----------------------------------*/
 #include <Adafruit_MCP23X17.h>        // For MCP23017 I/O buffers
